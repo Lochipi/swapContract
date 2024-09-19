b1993b7e06ff172f29cdbdef6a7be8aabbdc3091
Emase <corneliuslochipi@gmail.com>
2024-09-19 17:38:39 +0300

    updated test - deployment added

--- test/Swap.t.ts
+++ test/Swap.t.ts
@@ -9,29 +9,19 @@ describe("Swap", function () {
   // and reset Hardhat Network to that snapshot in every test.
   async function deployOneYearLockFixture() {
     // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount, fulfiller] = await hre.ethers.getSigners();
+    const [owner, otherAccount] = await hre.ethers.getSigners();
 
     const Swaping = await hre.ethers.getContractFactory("SwapToken");
     const swap = await Swaping.deploy();
 
-    // deploy the swap contract
-    const Token = await hre.ethers.getContractFactory("Token");
-    const guzToken = await Token.deploy("Guz Token", "GUZ", hre.ethers.parseEther("1000"));
-    const w3bToken = await Token.deploy("W3B Token", "W3B", hre.ethers.parseEther("1000"));
-
-     // Fund other accounts with tokens for testing purposes
-     await (guzToken as any).transfer(otherAccount.address, hre.ethers.parseEther("100"));
-     await (w3bToken as any).transfer(fulfiller.address, hre.ethers.parseEther("100"));
- 
-     return { swap, guzToken, w3bToken, owner, otherAccount, fulfiller };
+    return { swap, owner, otherAccount };
   }
 
   describe("Deployment", function () {
     it("Should set the right owner", async function () {
-      const { owner } = await loadFixture(deployOneYearLockFixture);
+      const { swap, owner } = await loadFixture(deployOneYearLockFixture);
 
       expect(await owner.address).to.equal(owner.address);
     });
   });
 });
-
